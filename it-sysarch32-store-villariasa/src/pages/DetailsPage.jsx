d285bd7ffcec89c93260dc044c9d53355c4e32f7
Joseph Lhee Villariasa <78625-22232078@SERVER2.DOMAIN>
2024-05-11 15:22:44 +0800

    Added my latest code

--- it-sysarch32-store-villariasa/src/pages/DetailsPage.jsx
+++ it-sysarch32-store-villariasa/src/pages/DetailsPage.jsx
@@ -4,11 +4,17 @@ import { doc, getDoc, collection, addDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import Header from "../components/Header";
 import db from "../configs/firebase";
+import { loadStripe } from '@stripe/stripe-js';
+
+// Load the Stripe.js library with your publishable API key
+const stripePromise = loadStripe('pk_test_51PF9H6H4YeXTNFtDcamCLIZ6XKRrXpxW3SxNGO40mdiqBHCCUo8eJiKAWgaaqpXFFJ4xcTyzI70RX8ixSqQZdeTm00zj8cYoCo'); // Replace with your publishable key
+
 
 function DetailsPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [isAddingToCart, setIsAddingToCart] = useState(false);
+  const [error, setError] = useState(null); // Define the error statese);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -53,6 +59,38 @@ function DetailsPage() {
       console.error("Error adding product to cart:", error);
     }
   };
+
+  const handleClick = async (productName, price) => {
+    const stripe = await stripePromise;
+  
+    // Send a request to the backend to create a checkout session
+    const response = await fetch('http://localhost:4000/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ productName, price }), // Send product name and price to the backend
+    });
+  
+    if (response.ok) {
+      // If the request is successful, retrieve the session ID from the response
+      const session = await response.json();
+  
+      // Redirect the user to the Stripe Checkout page using the session ID
+      const result = await stripe.redirectToCheckout({ sessionId: session.id });
+  
+      if (result.error) {
+        // If there is an error during the redirect, display the error message
+        setError(result.error.message);
+      }
+    } else {
+      // If there is an error creating the checkout session, display an error message
+      setError('Error creating checkout session');
+    }
+  };
+
+
+
 
   if (!product) {
     return <div>Loading...</div>;
@@ -110,6 +148,10 @@ function DetailsPage() {
                 <i className="bx bx-cart-add"></i>
                 Add to Cart
               </button>
+            </div>
+            <div className="add-to-cart2">
+              <button onClick={() => handleClick(product.name, product.value * 100)}>Checkout</button>
+              {error && <div>{error}</div>}
             </div>
           </section>
         </>
